97688f7c70653348c14f62e474f34fc46decdc45
Richard Butterworth <Richard Butterworth>
2017-03-31 00:54:27 +0100

    Initial commit - tutorial runthrough.

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BeerListContainer} from './components';
+
+ReactDOM.render(
+    <BeerListContainer/>, 
+    document.querySelector('#root'));
